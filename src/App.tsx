2fb2b8d57406839d38bf620d044972694922afaa
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-05-02 13:24:21 +0100

    Responsiveness for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect } from "react";
 import Docs from "./sections/Docs";
 import Features from "./sections/Features";
 import Footer from "./sections/Footer";
@@ -5,15 +6,25 @@ import Home from "./sections/Home";
 import Who from "./sections/who-using-it";
 
 function App() {
-  return (
-    <main className="relative m-0 p-0 bg-gradient-to-r from-[#330867] to-[#31a7bb]">
-      <Home />
-      <Features />
-      <Docs />
-      <Who />
-      <Footer />
-    </main>
-  );
+  if (window.innerWidth > 768) {
+    return (
+      <main className="relative m-0 p-0 bg-gradient-to-r from-[#330867] to-[#31a7bb]">
+        <Home />
+        <Features />
+        <Docs />
+        <Who />
+        <Footer />
+      </main>
+    );
+  } else {
+    return (
+      <div className="w-full text-center py-8">
+        <h1 className="text-2xl font-semibold capitalize">
+          Please use a larger screen
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
